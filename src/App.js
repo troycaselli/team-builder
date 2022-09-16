80f5a6c332021307a4128a02634e610948091444
Troy Caselli <troycaselli@Troys-MacBook-Air.local>
2022-09-16 09:13:56 -0700

    basic structure of form is completed

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import {useState} from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Team Members Form</h1>
+      <form>
+        <div className='container'>
+          <label>Name:</label>
+          <input type='text' placeholder='John Smith' />
+        </div>
+        <div className='container'>
+          <label>Email:</label>
+          <input type='email' placeholder='johnsmith@gmail.com' />
+        </div>
+        <div className='container'>
+          <label>Role:</label>
+          <select>
+          <option>---Select---</option>
+            <option>Backend</option>
+            <option>Frontend</option>
+            <option>Fullstack</option>
+            <option>Data Science</option>
+          </select>
+        </div>
+        <div className='container'>
+          <label>Weekly Commitment:</label>
+          <select>
+            <option>---Select---</option>
+            <option>Full Time</option>
+            <option>Part Time</option>
+            <option>None!</option>
+          </select>
+        </div>
+        <div>
+          <button className='submitButton'>Submit</button>
+        </div>
+      </form>
+      <section>
+        <h3>Members</h3>
+      </section>
+    </div>
+  );
+}
+
+export default App;
